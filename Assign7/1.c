91ee62e7d2dbf31582a7fbf2d943b09b8c59b51e
Somnath Chattaraj <somnathchattaraj5@gmail.com>
2024-06-18 21:20:46 +0530

    Update 1.c

--- Assign7/1.c
+++ Assign7/1.c
@@ -1,63 +1,30 @@
-// #include <stdio.h>
-// #include <math.h>
-
-// double func(double x) {
-//     return x*x*x - x*x + 2;
-// }
-
-// void bisection(double a, double b) {
-//     if (func(a) * func(b) >= 0) {
-//         printf("Incorrect a and b\n");
-//         return;
-//     }
-
-//     double c = a;
-
-//     while ((b-a) >= 0.0001) {
-//         c = (a+b)/2;
-
-//         if (func(c) == 0.0)
-//             break;
-
-//         else if (func(c)*func(a) < 0)
-//             b = c;
-//         else
-//             a = c;
-//     }
-//     printf("The value of root is : %.4f\n",c);
-// }
-
-// int main() {
-//     double a , b;
-//     scanf("%lf %lf", &a, &b);
-//     bisection(a, b);
-//     return 0;
-// }
-
-// Path: Assign7/2.c
 #include <stdio.h>
+#include <math.h>
 
-int fnc(int x) {
+double f(double x) {
     return x*x*x + x*x - 1;
 }
 
-int fc() {
-    int a = 0, b = 0, i=0;
-    while (a * b < 0) {
-        a = fnc(i);
-        b = fnc(i+1);
-        i++;
+void bisection(double a, double b, double tol) {
+    double c;
+    if (f(a) * f(b) >= 0) {
+        printf("Invalid interval\n");
+        return;
     }
-    return a,b;
+    while ((b-a) >= tol) {
+        c = (a+b)/2;
+        if (f(c) == 0.0)
+            break;
+        else if (f(c)*f(a) < 0)
+            b = c;
+        else
+            a = c;
+    }
+    printf("The root is: %.4lf\n", c);
 }
 
-int main () {
-    int a, b;
-    a, b = fc();
-    // printf("%d %d", a, b);
-    int mid = (a+b)/2;
-    if (fnc(mid) > 0) {
-
-    }
+int main() {
+    double a = 0, b = 1, tol = 0.0001;
+    bisection(a, b, tol);
     return 0;
 }
