c99017e41c8d372d193f8d65ed47cc55732ddc30
Somnath Chattaraj <somnathchattaraj5@gmail.com>
2024-06-18 21:23:05 +0530

    Update 4.c

--- Assign7/4.c
+++ Assign7/4.c
@@ -1,80 +1,79 @@
 #include <stdio.h>
+#include <math.h>
 
-#define N 3
-
-void swap_row(float m[N][N+1], int i, int j) {
-    for (int k=0; k<=N; k++) {
-        float temp = m[i][k];
-        m[i][k] = m[j][k];
-        m[j][k] = temp;
-    }
-}
-
-void gauss_elimination_pivot(float m[N][N+1]) {
-    for (int i=0; i<N; i++) {
-        int pivot = i;
-        for (int j=i+1; j<N; j++) {
-            if (m[j][i] > m[pivot][i])
-                pivot = j;
-        }
-        if (pivot != i)
-            swap_row(m, i, pivot);
-
-        for (int j=i+1; j<N; j++) {
-            float f = m[j][i] / m[i][i];
-            for (int k=0; k<=N; k++)
-                m[j][k] -= f * m[i][k];
-        }
-    }
-
-    float x[N];
-    for (int i=N-1; i>=0; i--) {
-        x[i] = m[i][N];
-        for (int j=i+1; j<N; j++) {
-            if (i != j)
-                x[i] = x[i] - m[i][j]*x[j];
-        }
-        x[i] = x[i] / m[i][i];
-    }
-
-    printf("\nSolution: ");
-    for (int i=0; i<N; i++)
-        printf("%f ", x[i]);
-    printf("\n");
-}
-
-void gauss_jordan(float m[N][N+1]) {
-    for (int i=0; i<N; i++) {
-        for (int j=0; j<N; j++) {
-            if (i != j) {
-                float ratio = m[j][i] / m[i][i];
-                for (int k=0; k<=N; k++)
-                    m[j][k] -= ratio * m[i][k];
+void gaussEliminationPivoting(double a[3][4]) {
+    int n = 3;
+    for (int i = 0; i < n-1; i++) {
+        for (int k = i+1; k < n; k++) {
+            if (fabs(a[i][i]) < fabs(a[k][i])) {
+                for (int j = 0; j <= n; j++) {
+                    double temp = a[i][j];
+                    a[i][j] = a[k][j];
+                    a[k][j] = temp;
+                }
             }
         }
     }
+    for (int i = 0; i < n-1; i++) {
+        for (int k = i+1; k < n; k++) {
+            double t = a[k][i] / a[i][i];
+            for (int j = 0; j <= n; j++) {
+                a[k][j] = a[k][j] - t * a[i][j];
+            }
+        }
+    }
+    double x[3];
+    for (int i = n-1; i >= 0; i--) {
+        x[i] = a[i][n];
+        for (int j = i+1; j < n; j++) {
+            x[i] = x[i] - a[i][j] * x[j];
+        }
+        x[i] = x[i] / a[i][i];
+    }
+    printf("Solution by Gauss Elimination with Pivoting:\n");
+    for (int i = 0; i < n; i++) {
+        printf("x[%d] = %lf\n", i, x[i]);
+    }
+}
 
-    printf("\nSolution: ");
-    for (int i=0; i<N; i++)
-        printf("%f ", m[i][N] / m[i][i]);
-    printf("\n");
+void gaussJordan(double a[3][4]) {
+    int n = 3;
+    for (int i = 0; i < n; i++) {
+        if (a[i][i] == 0.0) {
+            printf("Mathematical Error!\n");
+            return;
+        }
+        for (int j = 0; j < n; j++) {
+            if (i != j) {
+                double ratio = a[j][i] / a[i][i];
+                for (int k = 0; k <= n; k++) {
+                    a[j][k] = a[j][k] - ratio * a[i][k];
+                }
+            }
+        }
+    }
+    double x[3];
+    for (int i = 0; i < n; i++) {
+        x[i] = a[i][n] / a[i][i];
+    }
+    printf("Solution by Gauss-Jordan Method:\n");
+    for (int i = 0; i < n; i++) {
+        printf("x[%d] = %lf\n", i, x[i]);
+    }
 }
 
 int main() {
-    float m[N][N+1] = {{3, 2, -4, 3}, {2, 3, 3, 15}, {5, -3, 1, 14}};
-    int choice;
-    printf("1. Gauss Elimination with Pivoting\n2. Gauss Jordan\nEnter your choice: ");
-    scanf("%d", &choice);
-    printf("%d", choice);
-    switch(choice) {
-        case 1:
-            gauss_elimination_pivot(m);
-            break;
-        case 2:
-            gauss_jordan(m);
-            break;
-        default:
-            printf("Invalid choice\n");
-    }
+    double a1[3][4] = {
+        {1, 1, 1, 3},
+        {2, 3, 1, 6},
+        {1, -1, -1, -3}
+    };
+    double a2[3][4] = {
+        {1, 1, 1, 3},
+        {2, 3, 1, 6},
+        {1, -1, -1, -3}
+    };
+    gaussEliminationPivoting(a1);
+    gaussJordan(a2);
     return 0;
 }
