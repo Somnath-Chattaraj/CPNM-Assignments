2df450eb444ab014e0d28563248f5ce661af630b
Somnath Chattaraj <somnathchattaraj5@gmail.com>
2024-06-18 21:32:07 +0530

    Update 4.c

--- Assign7/4.c
+++ Assign7/4.c
@@ -3,6 +3,8 @@
 
 void gaussEliminationPivoting(double a[3][4]) {
     int n = 3;
+
+    // Partial pivoting
     for (int i = 0; i < n-1; i++) {
         for (int k = i+1; k < n; k++) {
             if (fabs(a[i][i]) < fabs(a[k][i])) {
@@ -14,6 +16,8 @@ void gaussEliminationPivoting(double a[3][4]) {
             }
         }
     }
+
+    // Gauss Elimination
     for (int i = 0; i < n-1; i++) {
         for (int k = i+1; k < n; k++) {
             double t = a[k][i] / a[i][i];
@@ -22,6 +26,8 @@ void gaussEliminationPivoting(double a[3][4]) {
             }
         }
     }
+
+    // Back substitution
     double x[3];
     for (int i = n-1; i >= 0; i--) {
         x[i] = a[i][n];
@@ -30,6 +36,8 @@ void gaussEliminationPivoting(double a[3][4]) {
         }
         x[i] = x[i] / a[i][i];
     }
+
+    // Printing the solution
     printf("Solution by Gauss Elimination with Pivoting:\n");
     for (int i = 0; i < n; i++) {
         printf("x[%d] = %lf\n", i, x[i]);
@@ -38,6 +46,8 @@ void gaussEliminationPivoting(double a[3][4]) {
 
 void gaussJordan(double a[3][4]) {
     int n = 3;
+
+    // Applying Gauss Jordan Elimination
     for (int i = 0; i < n; i++) {
         if (a[i][i] == 0.0) {
             printf("Mathematical Error!\n");
@@ -52,17 +62,30 @@ void gaussJordan(double a[3][4]) {
             }
         }
     }
+
+    // Obtaining the solution
     double x[3];
     for (int i = 0; i < n; i++) {
         x[i] = a[i][n] / a[i][i];
     }
+
+    // Printing the solution
     printf("Solution by Gauss-Jordan Method:\n");
     for (int i = 0; i < n; i++) {
         printf("x[%d] = %lf\n", i, x[i]);
     }
 }
 
+void printMenu() {
+    printf("\nMenu:\n");
+    printf("1. Solve using Gauss Elimination with Pivoting\n");
+    printf("2. Solve using Gauss-Jordan Method\n");
+    printf("3. Exit\n");
+    printf("Enter your choice: ");
+}
+
 int main() {
+    int choice;
     double a1[3][4] = {
         {1, 1, 1, 3},
         {2, 3, 1, 6},
@@ -73,7 +96,26 @@ int main() {
         {2, 3, 1, 6},
         {1, -1, -1, -3}
     };
-    gaussEliminationPivoting(a1);
-    gaussJordan(a2);
+
+    do {
+        printMenu();
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                gaussEliminationPivoting(a1);
+                break;
+            case 2:
+                gaussJordan(a2);
+                break;
+            case 3:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice! Please try again.\n");
+                break;
+        }
+    } while (choice != 3);
+
     return 0;
 }
